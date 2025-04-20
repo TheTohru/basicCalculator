3ff701a17b3a6e6c4c6c3d956b7e342ab2379235
TheTohru <yutresuf09@gmail.com>
2025-04-20 20:05:22 +0300

    Subtraction.java

--- Subtraction.java
+++ Subtraction.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//Please check README.md before using codes!
 public class Subtraction {
     public void subtraction() {
         Scanner scanner = new Scanner(System.in);
