d8241a671efce0aae560d66a135bc335a4aec9e0
TheTohru <yutresuf09@gmail.com>
2025-04-20 19:56:24 +0300

    Addition.java

---
+++ Addition.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class addition {
+    public void Addition() {
+        try {
+            System.out.println("addition selected, please give number one and number two:");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("give a number: ");
+            int s1 = scanner.nextInt();
+            System.out.print("give a number: ");
+            int s2 = scanner.nextInt();
+
+            int result = s1 + s2;
+            System.out.println("result: " + result);
+            System.out.println("two entered numbers: " + s1 + " and " + s2);
+            System.out.println("addition completed.");
+
+            System.out.println("do you want to try again? (y/n)");
+            String choice = scanner.next();
+            if (choice.equals("y")) {
+                addition A = new addition();
+                A.Addition();
+            } else if (choice.equals("n")) {
+                System.out.println("calculator shuts down");
+            } else {
+                System.out.println("wrong cohice");
+            }
+        }
+        catch(Exception e) {
+
+            System.out.println("error: " + e.getMessage());
+        }
+
+    }
+}
